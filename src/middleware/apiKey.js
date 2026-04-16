d097fde18c134ab6c0a64a038274e043e3d0649a
shweta jayakumar <95401491+Shwetajayakumar02@users.noreply.github.com>
2026-04-16 21:21:56 +0530

    Create apiKey.js

---
+++ src/middleware/apiKey.js
@@ -0,0 +1,28 @@
+const pool = require('../config/db');
+const bcrypt = require('bcrypt');
+
+module.exports = async (req, res, next) => {
+  try {
+    const apiKey = req.headers['x-api-key'];
+
+    if (!apiKey) {
+      return res.status(401).json({ message: 'API key missing' });
+    }
+
+    const result = await pool.query(
+      'SELECT * FROM api_keys WHERE revoked = false'
+    );
+
+    for (let row of result.rows) {
+      const isValid = await bcrypt.compare(apiKey, row.key_hash);
+      if (isValid) {
+        req.appId = row.app_id;
+        return next();
+      }
+    }
+
+    return res.status(403).json({ message: 'Invalid API key' });
+  } catch (err) {
+    return res.status(500).json({ message: 'Auth error' });
+  }
+};
