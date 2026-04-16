98f899ae5fa2bff784e538e736343b92f1deed2d
shweta jayakumar <95401491+Shwetajayakumar02@users.noreply.github.com>
2026-04-16 21:35:09 +0530

    Update basic.test.js

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,5 +1,17 @@
-describe('Basic Test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+const request = require('supertest');
+const app = require('../src/server');
+
+describe('API Tests', () => {
+  it('should fail without API key', async () => {
+    const res = await request(app).post('/api/analytics/collect');
+    expect(res.statusCode).toBe(401);
+  });
+
+  it('should register app', async () => {
+    const res = await request(app)
+      .post('/api/auth/register')
+      .send({ name: 'test', email: 'test@test.com' });
+
+    expect(res.statusCode).toBe(200);
   });
 });
